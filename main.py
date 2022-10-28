3438bb4f47461328274d67ed4a8aada77351f218
João Pedro <5117.joaopedro@gmail.com>
2022-10-27 23:41:41 -0300

    adicionei outros dois coletaveis

--- main.py
+++ main.py
@@ -13,37 +13,35 @@ bg_img = pygame.transform.scale(bg_img, (width, height))
 
 pygame.display.set_caption("Alagoaninho Adventures")
 
-pontuacao = 0
-
-# puxando fontes:
-fonte = pygame.font.Font('freesansbold.ttf', 20)
-
-# criando a escrita:
-texto = fonte.render("pontuacao: " + str(pontuacao),
-                     True, (255, 255, 255), (0, 0, 0))
-
-# criando a superficie:
-superficie_escrever = texto.get_rect()
-
-superficie_escrever.center = (250, 20)
+pontuacao1 = 0
+pontuacao2 = 0
+pontuacao3 = 0
 
 # posicao do objeto
 x = 1110
 y = 520
 
-# item coletavel 1:
-cx = randint(0, 500)
-cy = randint(0, 500)
+altura_colataveis = 15
+largura_coletaveis = 15
 
-hc = 10
-wc = 10
+# item coletavel 1:
+cx1 = randint(0, 500)
+cy1 = randint(0, 500)
+
+# item coletavel 2:
+cx2 = randint(0, 500)
+cy2 = randint(0, 500)
+
+# item coletavel 3:
+cx3 = randint(0, 500)
+cy3 = randint(0, 500)
 
 # dimensionando o objeto
 width = 20
 height = 20
 
 # velocidade do bloco
-vel = 3.5
+vel = 5
 
 # pygame rodando
 run = True
@@ -51,9 +49,13 @@ run = True
 
 objeto = pygame.Rect(x, y, width, height)
 
-coletavel = pygame.Rect(cx, cy, wc, hc)
+coletavel1 = pygame.Rect(cx1, cy1, largura_coletaveis, altura_colataveis)
 
-# loop infinito
+coletavel2 = pygame.Rect(cx2, cy2, largura_coletaveis, altura_colataveis)
+
+coletavel3 = pygame.Rect(cx3, cy3, largura_coletaveis, altura_colataveis)
+
+# loop do game
 while run:
     # aplica o background
     win.blit(bg_img, (0, 0))
@@ -69,9 +71,6 @@ while run:
 
             # saindo do loop
             run = False
-
-    if pontuacao == 10:
-        run = False
 
     keys = pygame.key.get_pressed()
 
@@ -98,30 +97,58 @@ while run:
         # incrementando no eixo y
         objeto.y += vel
 
-    coletou = objeto.colliderect(coletavel)
+    coletou1 = objeto.colliderect(coletavel1)
+    coletou2 = objeto.colliderect(coletavel2)
+    coletou3 = objeto.colliderect(coletavel3)
 
-    # se tiver coletado:
-    if coletou:
-        coletavel.x = randint(0, 500)
-        coletavel.y = randint(0, 500)
-        pontuacao += 1
+    # se tiver coletado o coletavel 1:
+    if coletou1:
+        coletavel1.x = randint(0, 500)
+        coletavel1.y = randint(0, 500)
+        pontuacao1 += 1
+
+    # se tiver coletado o coletavel 2:
+    if coletou2:
+        coletavel2.x = randint(0, 500)
+        coletavel2.y = randint(0, 500)
+        pontuacao2 += 1
+
+    # se tiver coletado o coletavel 3:
+    if coletou3:
+        coletavel3.x = randint(0, 500)
+        coletavel3.y = randint(0, 500)
+        pontuacao3 += 1
 
     # escrevendo na tela:
     # puxando fontes:
-    fonte = pygame.font.Font('freesansbold.ttf', 32)
+    fonte = pygame.font.Font('freesansbold.ttf', 25)
 
     # criando a escrita:
-    texto = fonte.render("Pontuação: " + str(pontuacao),
-                         True, (0, 0, 0))
+    texto1 = fonte.render("Branco: " + str(pontuacao1),
+                          True, (0, 0, 0))
+    texto2 = fonte.render("Rosa: " + str(pontuacao2),
+                          True, (0, 0, 0))
+    texto3 = fonte.render("Marrom: " + str(pontuacao3),
+                          True, (0, 0, 0))
 
     # criando a superficie:
-    superficie_escrever = texto.get_rect()
+    escrever1 = texto1.get_rect()
+    escrever2 = texto2.get_rect()
+    escrever3 = texto3.get_rect()
 
-    superficie_escrever.center = (350, 20)
-    win.blit(texto, superficie_escrever)
+    escrever1.center = (350, 20)
+    win.blit(texto1, escrever1)
 
-    # desenhando o objeto que se move e o coletavel
-    pygame.draw.rect(win, (255, 255, 255), coletavel)
+    escrever2.center = (600, 20)
+    win.blit(texto2, escrever2)
+
+    escrever3.center = (850, 20)
+    win.blit(texto3, escrever3)
+
+    # desenhando o objeto que se move e o coletavel1
+    pygame.draw.rect(win, (255, 255, 255), coletavel1)
+    pygame.draw.rect(win, (255, 192, 203), coletavel2)
+    pygame.draw.rect(win, (139, 69, 19), coletavel3)
 
     pygame.draw.rect(win, (255, 0, 0), objeto)
 
