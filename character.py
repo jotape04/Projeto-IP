9a828e89e2b046715b88920f7c4d5b2893a9437e
João Pedro <5117.joaopedro@gmail.com>
2022-11-05 20:34:49 -0300

    feat: adicionei as plataformas

--- character.py
+++ character.py
@@ -8,19 +8,22 @@ ATT = 7
 class Alagoano(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-parado.png")
-        self.rect = self.image.get_rect(center = (1116, 526))
+        self.image = pygame.image.load(
+            "sprites/alagoaninho/alagoaninho-parado.png")
+        self.rect = self.image.get_rect(center=(1116, 526))
         self.keys = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
-        self.direction = 1 # 1 é direita 0 é esquerda
+        self.direction = 1  # 1 é direita 0 é esquerda
         self.walk = 0
         self.jumpi = 0
         self.lock = 0
 
     def base(self):
         if self.direction == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-parado(IN).png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-parado(IN).png")
         elif self.direction == 1:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-parado.png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-parado.png")
 
     def right(self):
         self.direction = 0
@@ -28,30 +31,78 @@ class Alagoano(pygame.sprite.Sprite):
             self.rect.x += VEL
 
         if self.walk < ATT and self.jumpi == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-andando(IN).png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-andando(IN).png")
             self.walk += 1
         elif self.walk == ATT and self.jumpi == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-parado(IN).png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-parado(IN).png")
             self.walk += 1
-        elif self.walk == 8 and self.jumpi == 0: 
+        elif self.walk == 8 and self.jumpi == 0:
             self.walk = 0
-    
+
     def left(self):
         self.direction = 1
         if (self.rect.x > 20):
             self.rect.x -= VEL
 
         if self.walk < ATT and self.jumpi == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-andando.png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-andando.png")
             self.walk += 1
         elif self.walk == ATT and self.jumpi == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-parado.png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-parado.png")
             self.walk += 1
-        elif self.walk == 8 and self.jumpi == 0: 
+        elif self.walk == 8 and self.jumpi == 0:
             self.walk = 0
-        
+
     def down(self):
-        if(869>self.rect.x >317 and 294 > self.rect.y > 288):
+        if (855 > self.rect.x > 300 and 300 > self.rect.y > 290):
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 1050 > self.rect.x > 765 and 430 < self.rect.y < 440:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 610 > self.rect.x > 480 and 430 < self.rect.y < 440:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 125 > self.rect.x > 45 and 425 < self.rect.y < 435:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 235 > self.rect.x > 155 and 340 < self.rect.y < 350:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 95 > self.rect.x > 25 and 290 < self.rect.y < 300:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 205 > self.rect.x > 125 and 220 < self.rect.y < 230:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 115 > self.rect.x > 35 and 130 < self.rect.y < 140:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 305 > self.rect.x > 265 and 160 < self.rect.y < 170:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 480 > self.rect.x > 405 and 130 < self.rect.y < 140:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 890 > self.rect.x > 845 and 160 < self.rect.y < 170:
+            self.jumpi = 0
+            self.lock = 0
+            return 0
+        elif 1070 > self.rect.x > 1000 and 130 < self.rect.y < 140:
             self.jumpi = 0
             self.lock = 0
             return 0
@@ -62,22 +113,26 @@ class Alagoano(pygame.sprite.Sprite):
             self.jumpi = 0
             self.lock = 0
             return 0
-    
+
     def up(self):
         self.lock = 1
         self.jumpi = 1
-        if 530 > self.rect.x > 368 and 360> self.rect.y:
+        if (570 > self.rect.x > 380 and 360 > self.rect.y > 300) or (895 > self.rect.x > 810 and 360 > self.rect.y > 300):
             pass
         elif self.rect.y > -5:
-            self.rect.y -= VEL* 1.38
+            self.rect.y -= VEL * 1.38
 
         if self.direction == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-pulando(IN).png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-pulando(IN).png")
         elif self.direction == 1:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-pulando.png")
-    
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-pulando.png")
+
     def updatejump(self):
         if self.direction == 0:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-pulando(IN).png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-pulando(IN).png")
         elif self.direction == 1:
-            self.image = pygame.image.load("sprites/alagoaninho/alagoaninho-pulando.png")
+            self.image = pygame.image.load(
+                "sprites/alagoaninho/alagoaninho-pulando.png")
